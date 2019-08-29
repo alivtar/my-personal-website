3e029354d15635e23466e2b81929d6945b66fafb
Alivtar <ali.angori007@gmail.com>
2019-08-29 22:55:33 +0430

    added urls of MEDIA FILES to urlpatterns in 'portfolio/urls.py' so they can be loaded when requested

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .settings import MEDIA_URL, MEDIA_ROOT
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(MEDIA_URL, document_root = MEDIA_ROOT)
