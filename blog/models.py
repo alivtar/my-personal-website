d49da2e63253c2502f648fead187d752264d182d
Alivtar <ali.angori007@gmail.com>
2019-08-30 14:54:47 +0430

    Blog app up and running with 'Blogs' model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Blogs(models.Model):
+    title = models.CharField(max_length = 100)
+    publication_date = models.DateTimeField(auto_now = False, auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now = True, auto_now_add = False)
+    text = models.TextField()
+    image = models.ImageField(upload_to = 'images/')
