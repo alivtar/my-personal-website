2b235d28166c660116b771c332c0b590b70524a7
Alivtar <ali.angori007@gmail.com>
2019-09-01 02:49:08 +0430

    gathering info from 'jobs_Job' table to the code

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    jobs = Job.objects
+    return render(request, 'home.html', {'jobs' : jobs})
